68c42202abd828f4bdd516fcf464b926cea941d3
Tefgoo <tefgoov@gmail.com>
2023-04-14 22:08:31 -0500

    Window Monster
    Now Trigger for window monster is working with light and sound

---
+++ Assets/_Scripts/LightTriggerScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class LightTriggerScript : MonoBehaviour
+{
+    public Light lightToActivate;
+    public AudioClip soundToPlay;
+
+    private bool hasPlayedSound = false;
+
+    private void Start()
+    {
+        lightToActivate.enabled = false;
+        hasPlayedSound = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!hasPlayedSound)
+            {
+                AudioSource.PlayClipAtPoint(soundToPlay, transform.position);
+                hasPlayedSound = true;
+            }
+
+            StartCoroutine(TurnLightOnAndOff());
+        }
+    }
+
+    private IEnumerator TurnLightOnAndOff()
+    {
+        lightToActivate.enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        lightToActivate.enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        lightToActivate.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        lightToActivate.enabled = false;
+    }
+}
