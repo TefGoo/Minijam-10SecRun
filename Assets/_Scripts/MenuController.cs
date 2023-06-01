e8afa2755a847b34278003c5913630c3103bbc8c
Tefgoo <tefgoov@gmail.com>
2023-05-31 20:02:25 -0500

    Lighting and Menu improve
    Changed Start menu function and fixed the lighting in the apartment, removed lamps and made realtime lights instead of baked.

--- Assets/_Scripts/MenuController.cs
+++ Assets/_Scripts/MenuController.cs
@@ -4,8 +4,10 @@ public class MenuController : MonoBehaviour
 {
     public MonoBehaviour playerMovement; // Reference to the player's movement script (the component that controls movement)
     public GameObject menuUI; // Reference to the Canvas GameObject containing the menu UI
+    public GameObject panelUI; // Reference to the panel GameObject that should be activated/deactivated
 
     private bool isMenuActive = true;
+    private bool isPanelActive = false;
     private float previousTimeScale;
 
     private void Start()
@@ -13,17 +15,42 @@ public class MenuController : MonoBehaviour
         // Initially, disable the player's movement script and activate the menu
         playerMovement.enabled = false;
         menuUI.SetActive(true);
+        panelUI.SetActive(false); // Deactivate the panel
         previousTimeScale = Time.timeScale;
         Time.timeScale = 0f; // Stop the time
+
+        Cursor.lockState = CursorLockMode.None; // Unlock the cursor
+        Cursor.visible = true; // Show the cursor
     }
 
-    private void Update()
+    public void StartGame()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) // Adjust the input key according to your preference
+        isMenuActive = false;
+        ToggleMenu();
+    }
+
+    public void TogglePanel()
+    {
+        isPanelActive = !isPanelActive;
+        panelUI.SetActive(isPanelActive);
+
+        // Adjust the game state based on panel activation
+        if (isPanelActive)
         {
-            isMenuActive = !isMenuActive;
-            ToggleMenu();
+            Time.timeScale = 0f; // Stop the time
+            playerMovement.enabled = false;
         }
+        else
+        {
+            Time.timeScale = previousTimeScale; // Restore the previous time scale
+            playerMovement.enabled = true;
+        }
+    }
+
+    public void QuitGame()
+    {
+        // Implement your code to quit the game here
+        Application.Quit();
     }
 
     private void ToggleMenu()
@@ -44,17 +71,5 @@ public class MenuController : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
             Cursor.visible = false; // Hide the cursor
         }
-    }
-
-    public void ResumeGame()
-    {
-        isMenuActive = false;
-        ToggleMenu();
-    }
-
-    public void QuitGame()
-    {
-        // Implement your code to quit the game here
-        Application.Quit();
     }
 }
